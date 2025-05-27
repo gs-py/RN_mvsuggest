577e7e6472768b3fd3cc6309c1468f4c82862fa3
Gladwin <gladwins@taphubs.org>
2025-05-27 11:59:16 +0530

    feat: add nativewind and tailwindcss configuration
    - Add nativewind and tailwindcss dependencies
    - Configure babel, metro, and tailwind for nativewind integration
    - Update tsconfig to include nativewind types
    - Add global CSS file with tailwind directives
    - Modify layout and components to use tailwind classes

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,20 @@
+import { useState } from "react";
 import { Text, View } from "react-native";
 
 export default function Index() {
+  const [count, setCount] = useState(0);
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center w-[100dvw] h-[100dvh] bg-indigo-600">
+      <Text
+        onPress={() => {
+          setCount(count + 1);
+        }}
+        className="bg-red-500  min-w-4 text-center p-3"
+      >
+        {count}
+      </Text>
+      <View><Text>new name here</Text></View>
+      <Text className=" text-primary bg-orange-400">chnagibnf those text</Text>
     </View>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
+  content: ["./App.tsx", "./app/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#030014",
+        secondary: "#606060",
+        background: "#F5F5F5",
+        text: "#333333",
+      },
+    },
+  },
+  plugins: [],
+};
